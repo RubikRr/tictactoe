e56a974a8a420b8b94b48068d3c58bd20343e9f1
Ukropchik <vova.chakalov.2003@gmail.com>
2023-03-31 09:35:36 +0300

    добавил нужные переменные в js и функцию для получения ранломного значения

--- tictactoe/wwwroot/js/site.js
+++ tictactoe/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+var arr, arr_events = [], win_block, winner, again, winning, game;
 
+var comp_sym = "o";
+var user_sym = "x";
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 // Write your JavaScript code.
